6d055f8c8d57e10116b4345c66752576faa2c330
swa1245 <swarajpatil2123@gmail.com>
2025-07-08 15:15:05 +0530

    dopne

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,49 @@
-import Canvas from "./(components)/Canvas";
+"use client";
+import gsap from "gsap";
+import { useEffect } from "react";
 
 export default function Home() {
-  const canvasDetails = {
-    startIndex: 0,
-    numImages: 5,
-    duration: 3000,
-    size: 100,
-    top: 20,
-    left: 30,
-    zIndex: 1,
-  };
+  const name = "Swaraj";
+  const halfIndex = Math.floor(name.length / 2); 
+
+  useEffect(() => {
+    gsap.from(gsap.utils.toArray(".g"), {
+      y: 70,
+      opacity: 0,
+      duration: 1,
+      delay: 0.5,
+      stagger: 0.3,
+    });
+
+    gsap.from(gsap.utils.toArray(".a"), {
+      y: 50,
+      opacity: 0,
+      duration: 0.6,
+      delay: 0.5,
+      stagger: 0.3,
+    });
+
+    gsap.from(gsap.utils.toArray(".b"), {
+      y: 50,
+      opacity: 0,
+      duration: 0.6,
+      delay: 0.5,
+      stagger: -0.3,
+    });
+  }, []);
+
   return (
-    <div>
-      <div className="w-full min-h-screen bg-black text-white">
-        <Canvas details={canvasDetails}/>
-      </div>
+    <div className="flex items-center min-h-screen justify-center text-center">
+      <h1 className="text-7xl overflow-hidden flex space-x-2">
+        {name.split("").map((char, index) => (
+          <span
+            className={`inline-block g ${index < halfIndex ? "a" : "b"}`}
+            key={index}
+          >
+            {char}
+          </span>
+        ))}
+      </h1>
     </div>
   );
 }
